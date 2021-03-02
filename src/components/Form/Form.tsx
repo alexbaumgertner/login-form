1d18cf6a4a6c5e2e8692d1d4ac0c5620ffb9be18
alexbaumgertner <alex.baumgertner@gmail.com>
2021-03-02 22:22:09 +0300

    Add button component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,12 @@
+import { Button } from '../../components'
+
 const Form = (): JSX.Element => {
   return (
     <form method="post" action="/login">
       <input type="text" name="login" placeholder="login or email" />
       <input type="password" name="password" placeholder="password" />
 
-      <button type="submit">Log in</button>
+      <Button type="submit">Log in</Button>
       <p>
         <a href="/login/restore">Forgot your password?</a>
       </p>
