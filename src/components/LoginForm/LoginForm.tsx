55020c80aa1271cc95aa5e100560184487a2ca4a
alexbaumgertner <alex.baumgertner@gmail.com>
2021-03-03 13:57:26 +0300

    Add other login methods

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -7,43 +7,48 @@ import styles from './LoginForm.module.css'
 const LoginForm: React.FunctionComponent = (): JSX.Element => {
   return (
     <form className={styles.form} method="post" action="/login/enter">
-      <input
-        className={styles.login}
-        type="text"
-        name="login"
-        placeholder="login or email"
-      />
-      <input
-        className={styles.password}
-        type="password"
-        name="password"
-        placeholder="password"
-      />
+      <div className={styles.methods}>
+        <div className="name_password">
+          <input
+            className={styles.login}
+            type="text"
+            name="login"
+            placeholder="login or email"
+          />
+          <input
+            className={styles.password}
+            type="password"
+            name="password"
+            placeholder="password"
+          />
 
-      <div className={styles.captcha} />
+          <div className={styles.captcha} />
 
-      <div className={styles.enter}>
-        <Button className={styles.submit} type="submit">
-          Log in
-        </Button>
+          <div className={styles.enter}>
+            <Button className={styles.submit} type="submit">
+              Log in
+            </Button>
 
-        <label htmlFor="remember_me">
-          <span>Remember me</span>
-          <input type="checkbox" name="remember_me" id="remember_me" />
-        </label>
-      </div>
+            <label htmlFor="remember_me">
+              <span>Remember me</span>
+              <input type="checkbox" name="remember_me" id="remember_me" />
+            </label>
+          </div>
 
-      <div className={styles.other}>
-        <p>
-          <Link href="/login/restore">
-            <a>Forgot your password?</a>
-          </Link>
-        </p>
-        <p>
-          <Link href="/login/register">
-            <a>Register</a>
-          </Link>
-        </p>
+          <div className={styles.other}>
+            <p>
+              <Link href="/login/restore">
+                <a>Forgot your password?</a>
+              </Link>
+            </p>
+            <p>
+              <Link href="/login/register">
+                <a>Register</a>
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className="oauth">VK FB INST TW</div>
       </div>
     </form>
   )
